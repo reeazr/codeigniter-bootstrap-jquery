5412720a466e99184d57d3e556340bb9e7f766e4
Reeaz Ramoly <reeaz@ramoly.info>
2016-07-22 22:14:51 +0400

    -added htaccess for removing index.php from url -integrated boostrap into the codeigniter views

---
+++ application/views/sample_content.php
@@ -0,0 +1,27 @@
+    <!-- Begin page content -->
+    <div class="container">
+      <div class="page-header">
+        <h1>Sticky footer with fixed navbar</h1>
+      </div>
+      <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body > .container</code>.</p>
+      <p>Back to <a href="../sticky-footer">the default sticky footer</a> minus the navbar.</p>
+	  
+		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		<p>This is a combination of CodeIgniter, Bootstrap and JQuery already packed for you. This will quickstart your projects instead of manually doing this same tasks over and over! Here's what has been packed for you:</p>
+		<code>
+		<ul>
+		<li>CodeIgniter v3.0.6</li>
+		<li>Bootstrap 3.3.6</li>
+		<li>jQuery v1.10.2</li>
+		</ul>
+		</code>
+		<p>If you would like to edit this page you'll find it located at:</p>
+		<code>application/views/welcome_message.php</code>
+
+		<p>The corresponding controller for this page is found at:</p>
+		<code>application/controllers/Welcome.php</code>
+
+		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>	  
+    </div>
+
+	
